48af22729b048119a37eab38f73eb965a4a09dd9
fmbic <fedkamedved@gmail.com>
2025-12-23 01:13:09 +0300

    need to change the array because they are fixed

--- quizapp/src/Main.java
+++ quizapp/src/Main.java
@@ -17,6 +17,11 @@ public static void main (String[] args)
     for(int i = 0; i < obj.questions.length; i++)
     {
         System.out.println(i +" "+ obj.questions[i]);
+        for (int j = 0; j < obj.options[i].length; j++) {
+            System.out.print(obj.options[j]);
+        }
+
     }
+
 
 }
