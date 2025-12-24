c7adca97f7f97226c011ed8f1bd1d3af6cbeca96
fmbic <fedkamedved@gmail.com>
2025-12-24 23:57:10 +0300

    stuck again

--- quizapp/src/mapper.java
+++ quizapp/src/mapper.java
@@ -2,26 +2,7 @@ import java.util.*;
 
 public class mapper {
 
-    String [] allQuestions  = {"Компьютерные сети это сети: ",
-            "В режиме коммутации каналов сохранение очередности передаваемой информации: "};
-    List<String> questionsList = new ArrayList<>();
 
+    ArrayList<questions> allQuestions = new ArrayList<>();
 
-
-    public void firstOptions () {
-        ArrayList <String> options = new ArrayList<>();
-        options.add("1. C коммутацией пакетов");
-        options.add("2. C коммутацией каналов");
-        String[] optionsList = options.toArray(new String[0]);
-        for(String option : options) {System.out.println(Arrays.toString(optionsList));}}
-
-
-
-    public void secondOptions () {
-        ArrayList <String> options = new ArrayList<>();
-        options.add("1. Обеспечиваются");
-        options.add("2. Не обеспечиваются");
-        for(String option : options) {System.out.println(option);}}
-
-    public void shuffler () {Collections.shuffle(questionsList);}
 }
