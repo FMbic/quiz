73e69d8007df961d6a4096bb2224337bbb511a43
fmbic <fedkamedved@gmail.com>
2025-12-25 23:34:41 +0300

    not much to commit

--- quizapp/src/Main.java
+++ quizapp/src/Main.java
@@ -5,10 +5,12 @@ public static void main (String[] args)
     Scanner answer = new Scanner(System.in);
     int correctAnswer = 0;
     int guess;
-    //questions q1 = new questions("Компьютерные сети это сети: ",
-            //Arrays.asList("1. C коммутацией пакетов","2. C коммутацией каналов"),0);
-    //questions q2 = new questions("В режиме коммутации каналов сохранение очередности передаваемой информации: ",
-            //Arrays.asList("1. Обеспечиваются","2. Не обеспечиваются"),1);
+   mapper qe = new mapper();
+
+   for (questions qu : qe.allQuestions)
+   {
+       System.out.println(qu);
+   }
 
     //System.out.println(q1.getQuestion()+"\n"+ q1.getOptions());
     //answer.nextLine();
--- quizapp/src/mapper.java
+++ quizapp/src/mapper.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class mapper {
 
 
-    ArrayList<questions> allQuestions = new ArrayList<>();
 
+    questions q1 = new questions("Компьютерные сети это сети: ",
+            Arrays.asList("1. C коммутацией пакетов","2. C коммутацией каналов"),0);
+    questions q2 = new questions("В режиме коммутации каналов сохранение очередности передаваемой информации: ",
+            Arrays.asList("1. Обеспечиваются","2. Не обеспечиваются"),1);
+    Object ob = new Object();
+
+    ArrayList<questions> allQuestions = new ArrayList<>(ob.toString(q1,q2));
 }
